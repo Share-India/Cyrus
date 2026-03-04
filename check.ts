7a91c96610db96b52b32e357506c3f045c5882f6
Aditya Ladge <aditya.ladge@gmail.com>
2026-03-04 11:56:00 +0530

    chore: remove redundant test scripts, trigger files, and unused data

--- check.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import dotenv from 'dotenv'
-dotenv.config({ path: '.env.local' })
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
-supabase.from('submissions').select('*').limit(1).then(({ data }) => console.log(Object.keys(data[0] || {})))
